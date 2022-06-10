2e0ff033a402374305a0321eda88cf2327085fe5
Matpas97 <passarellim22@gmail.com>
2022-06-10 10:20:57 +0200

    jsnack 4

--- jsnack4/js/script.js
+++ jsnack4/js/script.js
@@ -4,4 +4,17 @@
 //Chiedi per 6 volte all’utente di inserire un numero,
 //se è dispari inseriscilo nell’array.
 
-// Crea
+// Creo un Array vuoto
+ let userService = [];
+
+ // Chiedo all'utente di inserire un numero 
+
+ for( let i = 0 ; i < 6 ; i++) {
+     let userNumber = parseInt(prompt("Inserisci un numero"));
+
+    if( userNumber % 2 !== 0) {
+        userService.push(userNumber);
+        
+    }
+ }
+ console.log(userService);
